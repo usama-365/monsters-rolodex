af44634bd1ed52307f5d4a382d80b408074bc481
usama-365 <usama-365@protonmail.com>
2022-10-27 17:46:43 +0500

    Created the rolodex app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchString: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchString: event.target.value.toLowerCase() });
+  }
+
+  render() {
+    // Filter the monsters based on search string
+    const filteredMonsters = this.state.monsters.filter(monster => monster.name.toLowerCase().includes(this.state.searchString));
+    return (
+      <div className='App'>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox className='search-box' placeholder='Search monster' onChangeHandler={this.onSearchChange.bind(this)}></SearchBox>
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import Card from "../card/card.component";
+
+import './card-list.styles.css';
+
+class CardList extends Component {
+    render() {
+        const { monsters } = this.props;
+        return (
+            <div className="card-list">
+                {monsters.map(monster => <Card key={monster.id} monster={monster}></Card>)}
+            </div>
+        );
+    }
+}
+
+export default CardList;
